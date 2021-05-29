ce1275b90c97b17b767f3f71dbb98aaaf1d19dd2
john <john.d.leonetti@gmail.com>
2021-05-28 17:12:58 -0700

    commit tree 6759b6ce5149d46a8e9561671964954cdc88e4d6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// const superbowlWin = (recordArray) => {
+//     let team = {
+//         year: '1969'
+//         result: 'w'
+//     }
+//     team.find
+//        // return this.team;
 
+// }
+
+function superbowlWin(recordArray){
+    // console.log("testing to see array")
+     //console.log(recordArray[1]["result"] == 'w')
+    //recordArray.find(element => console.log(`this is the element: ${element}`) > 10);
+    let winningRecord
+    recordArray.find(element => {
+       if (element['result'] === "W")
+    {
+        winningRecord = element['year'];
+    }
+        
+    });
+    return winningRecord
+}
+
+
+//let record = team.find(result => result === year);
